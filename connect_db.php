1d46f51083277711b002443d438833c6cc5be9b7
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-09-25 13:26:50 +0700

    Add files via upload

---
+++ connect_db.php
@@ -0,0 +1,13 @@
+<!-- <?php
+    if(!isset($_SESSION)){ 
+        session_start();
+    };
+
+    $host = "localhost";
+    $username = "root";
+    $dbname = "";
+    $password = "";
+
+    $base_url = '';
+    $db = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
+?> -->
---
+++ loginUserCek.php
@@ -0,0 +1,27 @@
+<!-- <?php
+    include './connect_db.php';
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $queryEmailCheck = $db->prepare("SELECT * FROM user WHERE email= :email");
+    $queryEmailCheck->bindParam(":email", $email);
+    $queryEmailCheck->execute();
+    
+    $user = $queryEmailCheck->fetch();
+    if($queryEmailCheck->rowCount() > 0){ 
+        $encrypted_password_salt = md5($password.$user['salt']);
+        if($user['pass'] === $encrypted_password_salt){ 
+            $_SESSION['id_user'] = $email;
+            header("location: ./loginUser.php");
+        } 
+        else{ 
+            $_SESSION['error_login_message'] = 'Email atau password salah';
+            header("location: ./loginUser.php");
+        }
+    } 
+    else{ 
+        $_SESSION['error_login_message'] = 'Email atau password salah';
+        header("location: ./loginUser.php");
+      }
+?> -->
