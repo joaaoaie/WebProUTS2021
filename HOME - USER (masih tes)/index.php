0d1c0a45ed782f3113ae380858dc18c9bb27b798
FlxNg <91363258+FlxNg@users.noreply.github.com>
2021-09-28 15:14:26 +0700

    Add files via upload

---
+++ HOME - USER (masih tes)/index.php
@@ -0,0 +1,75 @@
+<?php 
+    include './connect_db.php';
+
+    $query = $db->prepare("SELECT * FROM berita");
+    $query->execute();
+    $berita = $query->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Portal Berita - IF330</title>
+    <script src="https://cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="main.css" type="text/css">
+</head>
+
+<!-- Belom paham make ini -->
+<?php //include './nav(blmslsi).php' ?>
+<!-- <body>
+  <div id="cf" class="col-12 col-m-12">
+    <img class="top" src='./image/Economy.jpg'/>
+    <img class="top" src='./image/Sports.jpg'/>
+    <img class="top" src='./image/Politic.jpg'/>
+    <img class="top" src='./image/News.jpg'/>
+  </div>
+</body> -->
+
+<!-- Parameter GET buat category -->
+<?php error_reporting(0); $kategori =  $_GET['kategori']; ?>
+
+<body>
+  <!-- Sorting category -->
+  <div class="sorting"> 
+    <a href="./index.php" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">All</a>
+    <a href="./index.php?kategori=Politik" class="category" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">Politics</a>
+    <a href="./index.php?kategori=Food" class="category" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">Food</a>
+    <a href="./index.php?kategori=Ekonomi" class="category" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">Economy</a>
+  </div>
+
+  <!-- Buat nampilin berita -->
+  <div id="berita">
+    <?php if($kategori != null) { 
+         foreach($berita as $news) { 
+          if($kategori == $news['kategori']) {?>
+          <div id=<?=$news['idBerita']?> class="container">
+          <h3><?=$news['judul']?></h3>
+          <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
+          <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
+        </div>
+      <?php }}
+       } else { 
+           foreach($berita as $news) { ?>
+            <div id=<?=$news['idBerita']?> class="container">
+              <h3><?=$news['judul']?></h3>
+              <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
+              <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
+            </div>
+        <?php }
+           } ?>
+  </div>
+
+</body>
+
+</html>
+
