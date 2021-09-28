aeaeb509bceb0c53750fe2e37e4a6271efa3ea46
FlxNg <91363258+FlxNg@users.noreply.github.com>
2021-09-28 17:59:24 +0700

    Update index.php

--- home - user (tes)/index.php
+++ home - user (tes)/index.php
@@ -23,6 +23,42 @@
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="main.css" type="text/css"/>
     <style>
+      [class*="col-"] {
+          width: 100%;
+      }
+
+      @media only screen and (min-width: 600px) {
+          /*for tablets (ukuran layar lebih besar dari 600px)*/
+          .col-m-1 {width: 8.33%;}
+          .col-m-2 {width: 16.66%;}
+          .col-m-3 {width: 25%;}
+          .col-m-4 {width: 33.33%;}
+          .col-m-5 {width: 41.66%;}
+          .col-m-6 {width: 50%;}
+          .col-m-7 {width: 58.33%;}
+          .col-m-8 {width: 66.66%;}
+          .col-m-9 {width: 75%;}
+          .col-m-10 {width: 83.33%;}
+          .col-m-11 {width: 91.66%;}
+          .col-m-12 {width: 100%;}
+      }
+
+      @media only screen and (min-width: 768px) {
+          /*for desktop (ukuran layar lebih besar dari 768px)*/
+          .col-1 {width: 8.33%;}
+          .col-2 {width: 16.66%;}
+          .col-3 {width: 25%;}
+          .col-4 {width: 33.33%;}
+          .col-5 {width: 41.66%;}
+          .col-6 {width: 50%;}
+          .col-7 {width: 58.33%;}
+          .col-8 {width: 66.66%;}
+          .col-9 {width: 75%;}
+          .col-10 {width: 83.33%;}
+          .col-11 {width: 91.66%;}
+          .col-12 {width: 100%;}
+      }
+
        @keyframes cfFadeInOut {
         0% {
         opacity:1;
@@ -63,6 +99,7 @@
         animation-duration: 5s;
         animation-direction: alternate;
         }
+        
     </style>
 </head>
 
@@ -70,30 +107,30 @@
 <?php error_reporting(0); $kategori =  $_GET['kategori']; ?>
 
 <!-- Gambar di HOME -->
-<body>
+<body class="col-12 col-m-12">
   <div id="cf" class="col-12 col-m-12">
-  <?php if($kategori == null) { ?>
-    <img class="bottom" src='./image/News2.jpg'/>
-    <img class="top" src='./image/News1.jpg'/>
-  <?php } ?>
-  <?php if($kategori == "Politik") { ?>
-    <img class="bottom" src='./image/Politik2.jpg'/>
-    <img class="top" src='./image/Politik1.jpg'/>
-  <?php } ?>
-  <?php if($kategori == "Sports") { ?>
-    <img class="bottom" src='./image/Sports2.jpg'/>
-    <img class="top" src='./image/Sports3.jpg'/>
-  <?php } ?>
-  <?php if($kategori == "Food") { ?>
-    <img class="bottom" src='./image/Food2.jpg'/>
-    <img class="top" src='./image/Food1.jpg'/>
-  <?php } ?>
+    <?php if($kategori == null) { ?>
+      <img class="bottom" style="max-width: 100%;" src='./image/News2.jpg'/>
+      <img class="top" style="max-width: 100%;" src='./image/News1.jpg'/>
+    <?php } ?>
+    <?php if($kategori == "Politik") { ?>
+      <img class="bottom" style="max-width: 100%;" src='./image/Politik2.jpg'/>
+      <img class="top" style="max-width: 100%;" src='./image/Politik1.jpg'/>
+    <?php } ?>
+    <?php if($kategori == "Sports") { ?>
+      <img class="bottom" style="max-width: 100%;" src='./image/Sports2.jpg'/>
+      <img class="top" style="max-width: 100%;" src='./image/Sports3.jpg'/>
+    <?php } ?>
+    <?php if($kategori == "Food") { ?>
+      <img class="bottom" style="max-width: 100%;" src='./image/Food2.jpg'/>
+      <img class="top" style="max-width: 100%;" src='./image/Food1.jpg'/>
+    <?php } ?>
   </div>
 </body>
 
-<body>
+<body class="col-12 col-m-12">
   <!-- Sorting category -->
-  <div class="sorting"> 
+  <div class="sorting col-12 col-m-12" style="background-color: lightgray;"> 
     <a href="./index.php" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">All</a>
     <a href="./index.php?kategori=Politik" class="category" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">Politics</a>
     <a href="./index.php?kategori=Food" class="category" style="font-size: xx-large;color: red;display: inline-block;padding: 20px;">Food</a>
@@ -101,25 +138,30 @@
   </div>
 
   <!-- Buat nampilin berita -->
-  <div id="berita" class="col-12 col-m-12">
-    <?php if($kategori != null) { 
-         foreach($berita as $news) { 
-          if($kategori == $news['kategori']) {?>
-          <div id=<?=$news['idBerita']?> class="container">
-          <h3><?=$news['judul']?></h3>
-          <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
-          <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
-        </div>
-      <?php }}
-       } else { 
-           foreach($berita as $news) { ?>
+  <div class="col-12 col-m-12">
+    <div class="col-3 col-m-3"></div>
+    <div id="berita" class="col-9 col-m-9">
+      <?php if($kategori != null) { 
+          foreach($berita as $news) { 
+            if($kategori == $news['kategori']) {?>
             <div id=<?=$news['idBerita']?> class="container">
-              <h3><?=$news['judul']?></h3>
-              <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
-              <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
-            </div>
-        <?php }
-           } ?>
+            <img style="width: 100%; margin-top: 10px; margin-right: auto;" src='./image/News2.jpg'/>
+            <a style="font-size: xx-large;"><?=$news['judul']?></a><br>
+            <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
+            <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
+          </div>
+        <?php }}
+        } else { 
+            foreach($berita as $news) { ?>
+              <div id=<?=$news['idBerita']?> class="container">
+                <img style="width: 100%; margin-top: 10px; margin-right: auto;" src='./image/News2.jpg'/>
+                <a style="font-size: xx-large;"><?=$news['judul']?></a><br>
+                <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
+                <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
+              </div>
+          <?php }
+            } ?>
+    </div>
   </div>
 
 </body>
