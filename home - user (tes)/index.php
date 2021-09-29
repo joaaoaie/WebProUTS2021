23add8a4eee6b00bd1d488d3820939fd2817f079
joaaoaie <jonathan.octavien@student.umn.ac.id>
2021-09-30 00:10:40 +0700

    Update index.php

--- home - user (tes)/index.php
+++ home - user (tes)/index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="main.css" type="text/css"/>
+    <link rel="stylesheet" href="../assets/main.css" type="text/css"/>
     <style>
       [class*="col-"] {
           width: 100%;
