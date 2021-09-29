4c8add4cfbf2e5bdd10fb10cee2fc470bc211c42
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-09-29 17:30:09 +0700

    Update logout.php

--- Login_Regis/logout.php
+++ Login_Regis/logout.php
@@ -1,8 +1,6 @@
 <?php
     include './connect_db.php';
-
-    // Hapus session email biar dianggap logout
-    unset($_SESSION['id_user']); //buat session finished gtu jadi biar user nya berhasil log out trs nnti balik ke window log in lgi
+    unset($_SESSION['id_user']);
 
     header("location: $base_url");
 ?>
