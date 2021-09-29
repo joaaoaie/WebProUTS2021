90ff35f6ca6f3f21b79a1c4b71aaecf31d294a29
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-09-29 17:21:40 +0700

    Add files via upload

---
+++ Login_Regis/logout.php
@@ -0,0 +1,8 @@
+<?php
+    include './connect_db.php';
+
+    // Hapus session email biar dianggap logout
+    unset($_SESSION['id_user']); //buat session finished gtu jadi biar user nya berhasil log out trs nnti balik ke window log in lgi
+
+    header("location: $base_url/loginUser.php");
+?>
