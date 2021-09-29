5f5e276835e346737d10f115dd28c79c96312d7a
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-09-29 17:37:48 +0700

    Add files via upload

---
+++ Login-Regis/index.php
@@ -0,0 +1,28 @@
+<?php include './connect_db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <script type="text/javascript"> 
+            $(document).ready(function(){
+                $('#myModal').modal({
+                    keyboard: false,
+                    show: true,
+                    backdrop: 'static'
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <?php
+            if(!isset($_SESSION['id_user']))
+                include 'loginUser.php';
+            else{
+                if($_SESSION['admin'])
+                    include "base.php"; //Admin View - CRUD
+                else   
+                    include "base.php"; //User View - View Only
+            }
+        ?>
+    </body>
+</html>
