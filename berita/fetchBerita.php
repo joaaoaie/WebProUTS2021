8d4ca1a7a7d3f8041cdc6b25d1df4ea74f390930
joaaoaie <jonathan.octavien@student.umn.ac.id>
2021-09-30 00:37:57 +0700

    Update fetchBerita.php

--- berita/fetchBerita.php
+++ berita/fetchBerita.php
@@ -1,7 +1,11 @@
 <?php
   include '../connect_db.php';
 
-  // $idBerita = POST['idBerita'];
-  $queryBerita = $db->query("SELECT * FROM berita WHERE idBerita = ?");
-  $queryBerita->execute($idBerita);
+  // ambil dari sini, ini buat yang di home -> <a href="../berita/index.php?idBerita='. $berita['idBerita'] .'">...</a>
+  $idBerita = $_GET['idBerita'];
+  $queryBerita = $db->query("SELECT * FROM berita WHERE idBerita = :idBerita");
+  $queryBerita->bindParam(":idBerita", $idBerita);
+  $queryBerita->execute();
 
+  $berita = $queryBerita->fetch();
+  header("location: $base_url/berita/index.php");
