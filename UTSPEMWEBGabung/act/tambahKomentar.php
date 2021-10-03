e673bb2283cacae948f4053f1c04bd7fda1f2d41
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-03 21:41:19 +0700

    Delete UTSPEMWEBGabung directory

--- UTSPEMWEBGabung/act/tambahKomentar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../connect_db.php';
-
-    if(!isset($_SESSION)){ 
-        header("location: $base_url/loginRegisPage/loginUser.php");
-    };
-
-    $username = $_POST['username'];
-    $isi = $_POST['isi'];
