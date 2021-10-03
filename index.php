1f85cbba99efca12bc10580f6724796eb178de2c
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-03 16:30:36 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(!session_id()) 
-      session_start();
-          
-    // require_once './connect_db.php';
-    // require_once './Login-Regis/index.php'; //sesuain sama folder final
-    require_once './home - user (tes)/index.php'; //sesuain sama folder final
