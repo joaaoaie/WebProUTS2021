2f8b82615e36533a0e8189442c0aecf80e52b075
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-03 14:52:20 +0700

    Add files via upload

---
+++ loginRegisPage/addNewUser.php
@@ -0,0 +1,61 @@
+<?php
+    include './connect_db.php';
+
+    // Mengambil data dari database untuk dilakukan pengecekan di bawah
+    $query = $db->prepare("SELECT * FROM user");
+    $query->execute();
+    $a = $query->fetchall();
+
+    // Mengambil nilai dari submit form
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $tanggalLahir = $_POST['tanggalLahir'];
+    $jenisKelamin = $_POST['jenisKelamin'];
+    $cek = true;
+    $salt = "user";
+
+    if($username != "" && $email != "" && $pass != "" && $firstName != "" && $tanggalLahir != "" && $jenisKelamin != ""){
+        if($lastName == "")
+            $lastName = NULL;
+
+        $encrypted_password_salt = md5($pass . $salt);
+        foreach($a as $userAcc){
+            if($userAcc['username'] == $username || $userAcc['email'] == $email){
+                if($userAcc['username'] == $username && $userAcc['email'] == $email)
+                    $_SESSION['error_login_message'] = "Username and Email already taken and used";
+                else if($userAcc['username'] == $username)
+                    $_SESSION['error_login_message'] = "Username already taken";
+                else
+                    $_SESSION['error_login_message'] = "Email already used";
+
+                $cek = false;
+                header("location: $base_url/loginRegisPage/registrationUser.php");
+                break;
+            }
+        }
+        if($cek){
+            $queryInsert = $db->prepare("INSERT into user(username,email,pass,salt,firstName,lastName,tanggalLahir,jenisKelamin) 
+                                         values(:username,:email,:pass,:salt,:firstName,:lastName,:tanggalLahir,:jenisKelamin)");
+
+            $queryInsert->bindParam(':username', $username);
+            $queryInsert->bindParam(':email', $email);
+            $queryInsert->bindParam(':pass', $encrypted_password_salt); //password and salt included
+            $queryInsert->bindParam(':salt', $salt);
+            $queryInsert->bindParam(':firstName', $firstName);
+            $queryInsert->bindParam(':lastName', $lastName);
+            $queryInsert->bindParam(':tanggalLahir', $tanggalLahir);
+            $queryInsert->bindParam(':jenisKelamin', $jenisKelamin);
+
+            $queryInsert->execute();
+            header("location: $base_url/loginRegisPage/loginRegister.php"); //User diminta untuk login setelah melakukan registrasi
+        }
+    }
+    else{
+        $_SESSION['error_login_message'] = "Fill all required fields";
+        header("location: $base_url/loginRegisPage/registrationUser.php");
+    }
+?>
+
