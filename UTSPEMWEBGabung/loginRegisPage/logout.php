7a386ef5e86f9fa38f91ff95fca87af19b22cd31
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-04 21:23:54 +0700

    Delete UTSPEMWEBGabung directory

--- UTSPEMWEBGabung/loginRegisPage/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../connect_db.php';
-
-    // Hapus session email biar dianggap logout
-    unset($_SESSION['id_user']); //buat session finished gtu jadi biar user nya berhasil log out trs nnti balik ke window log in lgi
-    unset($_SESSION['admin']);
-    header("location: $base_url/index.php");
-?>
