80dbb06c6bfbea141342e3744061b01e664627c0
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-05 17:44:37 +0700

    Add files via upload

---
+++ UTSPEMWEBGabung/adminView/act/adddataberita.php
@@ -0,0 +1,20 @@
+<?php
+  include './connect_db.php';
+
+  $id = $_POST['id'];
+  $judul = $_POST['Judul'];
+  $kategori = $_POST['Kategori'];
+  $penulis = $_POST['Penulis'];
+  $konten = $_POST['Konten'];
+  $tanggal = $_POST['Tanggal'];
+  $gambar = $_POST['gambarBerita'];
+
+  $queryInsert = $db->prepare("INSERT INTO berita(idBerita,judul,kategori,gambar, penulis,tanggal,konten)
+              VALUES (?,?,?,?,?,?,?)");
+
+  $success = $queryInsert->execute([$id, $judul, $kategori, $gambar, $penulis, $tanggal, $konten]);
+
+  if ($success) {
+    header("location: $base_url/adminView/CRUD.php");
+  }
+?>
