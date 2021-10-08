38f4228fb9ff2d2b22e7fab2c112683e2f32e455
StevenGeraldi <88229272+StevenGeraldi@users.noreply.github.com>
2021-10-08 22:41:12 +0700

    UPDATE ACT
    Bisa add gambar ke database dan simpen ke directory, tapi edit belom bisa sementara

--- UTSPEMWEBGabung/adminView/act/adddataberita.php
+++ UTSPEMWEBGabung/adminView/act/adddataberita.php
@@ -1,13 +1,22 @@
 <?php
-  include './connect_db.php';
-
+  include '../../connect_db.php';
+  $user = $_GET['user'];
   $id = $_POST['id'];
   $judul = $_POST['Judul'];
   $kategori = $_POST['Kategori'];
   $penulis = $_POST['Penulis'];
   $konten = $_POST['Konten'];
   $tanggal = $_POST['Tanggal'];
-  $gambar = $_POST['gambarBerita'];
+  
+  if($_FILES['gambar']['name'] != NULL){
+      $gambar =  time() . '_' . $_FILES['gambar']['name'];
+      $target = '../../image/news/' . $gambar;
+      move_uploaded_file($_FILES['gambar']['tmp_name'], $target);
+  }
+  else if ($_FILES['gambar']['name'] == NULL)
+      $gambar =  NULL;
+  
+
 
   $queryInsert = $db->prepare("INSERT INTO berita(idBerita,judul,kategori,gambar, penulis,tanggal,konten)
               VALUES (?,?,?,?,?,?,?)");
@@ -15,6 +24,6 @@
   $success = $queryInsert->execute([$id, $judul, $kategori, $gambar, $penulis, $tanggal, $konten]);
 
   if ($success) {
-    header("location: $base_url/adminView/CRUD.php");
+    header("location: $base_url/adminView/CRUD.php?id=$user");
   }
 ?>
--- UTSPEMWEBGabung/adminView/act/addeditberita.php
+++ UTSPEMWEBGabung/adminView/act/addeditberita.php
@@ -1,14 +1,18 @@
 <?php
-    include './connect_db.php';
-    
+    include '../../connect_db.php';
+    $user = $_GET['user'];
     $id = $_POST['id'];
     $judul = $_POST['Judul'];
     $kategori = $_POST['Kategori'];
     $penulis = $_POST['Penulis'];
     $konten = $_POST['Konten'];
     $tanggal = $_POST['Tanggal'];
-    $gambar = $_POST['gambarBerita'];
+    if($_FILES['gambar']['name'] != NULL){
+        $gambar =  time() . '_' . $_FILES['gambar']['name'];
+        $target = '../../image/news/' . $gambar;
+        move_uploaded_file($_FILES['gambar']['tmp_name'], $target);
+    }
     $queryberita = $db->prepare('UPDATE berita SET judul=?, kategori=?, penulis=?, konten=?, tanggal=?, gambar=? WHERE idBerita=?');
     $queryberita->execute([$judul, $kategori, $penulis, $konten, $tanggal, $gambar, $id]);
-    header("location: $base_url/adminView/crud.php");
+    header("location: $base_url/adminView/crud.php?id=$user");
 ?>
