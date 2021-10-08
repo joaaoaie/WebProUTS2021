d6fb021723c695d01a333c19ae6e851975e8fed6
joaaoaie <jonathan.octavien@student.umn.ac.id>
2021-10-08 17:14:17 +0700

    Update tambahKomentar.php

--- UTSPEMWEBGabung/act/tambahKomentar.php
+++ UTSPEMWEBGabung/act/tambahKomentar.php
@@ -5,5 +5,12 @@
         header("location: $base_url/loginRegisPage/loginUser.php");
     };
 
-    $username = $_POST['username'];
+    $emailUname = $_POST['emailUname'];
     $isi = $_POST['isi'];
+
+    $queryCheck = $db->prepare("SELECT * FROM user WHERE (email= :email OR username= :username)");
+    $queryCheck->bindParam(":email", $emailUname);
+    $queryCheck->bindParam(":username", $emailUname);
+    $queryCheck->execute();
+
+    $inserQuery = $db->prepare("INSERT INTO komentar VALUES");
