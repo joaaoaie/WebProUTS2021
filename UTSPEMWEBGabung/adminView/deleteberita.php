36c3319843f65cc2f6a3375db9fd71c55995164c
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-09 22:12:18 +0700

    Delete UTSPEMWEBGabung directory

--- UTSPEMWEBGabung/adminView/deleteberita.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../connect_db.php';
-    $id = $_GET['id'];
-
-    $queryberita = $db->prepare("DELETE FROM berita WHERE idBerita=?");
-    $queryberita->execute([$id]);
-
-    header("location: $base_url/adminView/CRUD.php");
-?>
