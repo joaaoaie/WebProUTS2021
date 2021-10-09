1e8a0928137f48205ac9bb3317aaa1d021547ed8
FlxNg <91363258+FlxNg@users.noreply.github.com>
2021-10-09 07:37:02 +0700

    Update index.php

--- UTSPEMWEBGabung/index.php
+++ UTSPEMWEBGabung/index.php
@@ -93,18 +93,18 @@
     <?php if($kategori != null) { 
         foreach($berita as $news) { 
           if($kategori == $news['kategori']) {?>
-          <div id=<?=$news['idBerita']?> class="container col-7 col-m-7" data-aos="fade-up">
+          <div id=<?=$news['idBerita']?> class="container col-6 col-m-6" data-aos="fade-up">
           <img style="width: 100%; margin-top: 10px; margin-right: auto;height: auto;" src='./image/News2.jpg'/>
-          <a href="./detailBerita.php?judul=<?=$news['judul']?>" style="font-size: xx-large;"><?=$news['judul']?></a><br>
+          <a href="./detailBerita.php?judul=<?=$news['judul']?>" style="font-size: xx-large; text-decoration: none; color: black; font-weight: 550;"><?=$news['judul']?></a><br>
           <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
           <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
         </div>
       <?php }}
       } else { 
           foreach($berita as $news) { ?>
-            <div id=<?=$news['idBerita']?> class="container col-7 col-m-7" data-aos="fade-up">
+            <div id=<?=$news['idBerita']?> class="container col-6 col-m-6" data-aos="fade-up">
               <img style="width: 100%; margin-top: 10px; margin-right: auto; height: auto;" src='./image/News2.jpg'/>
-              <a href="./detailBerita.php?judul=<?=$news['judul']?>" style="font-size: xx-large;"><?=$news['judul']?></a><br>
+              <a href="./detailBerita.php?judul=<?=$news['judul']?>" style="font-size: xx-large; text-decoration: none; color: black; font-weight: 550;"><?=$news['judul']?></a><br>
               <h4 style="display: inline-block; padding-right: 30px;">Kategori : <b><?=$news['kategori']?></b></h4>
               <h4 style="display: inline-block;">Tanggal : <b><?=$news['tanggal']?></b></h4>
             </div>
