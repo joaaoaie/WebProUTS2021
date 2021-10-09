5ffd855c987931c695a7d298c5661c6be108c837
joaaoaie <jonathan.octavien@student.umn.ac.id>
2021-10-09 13:31:35 +0700

    Update tambahKomentar.php

--- UTSPEMWEBGabung/act/tambahKomentar.php
+++ UTSPEMWEBGabung/act/tambahKomentar.php
@@ -5,12 +5,19 @@
         header("location: $base_url/loginRegisPage/loginUser.php");
     };
 
-    $emailUname = $_POST['emailUname'];
+    $judul = $_POST['judul'];
+    $Username = $_POST['Username'];
     $isi = $_POST['isi'];
-
-    $queryCheck = $db->prepare("SELECT * FROM user WHERE (email= :email OR username= :username)");
-    $queryCheck->bindParam(":email", $emailUname);
-    $queryCheck->bindParam(":username", $emailUname);
-    $queryCheck->execute();
-
-    $inserQuery = $db->prepare("INSERT INTO komentar VALUES");
+    $idBerita = $_POST['idBerita'];
+    $idKomentar = $_POST['idKomentar'];
+    $date = date("Y-m-d");
+    
+    $insertQuery = $db->prepare("INSERT INTO komentar VALUES (:idBerita, :idKomentar, :username, :tanggal, :isi, 0)");
+    $insertQuery->bindParam(":idBerita", $idBerita);
+    $insertQuery->bindParam(":idKomentar", $idKomentar);
+    $insertQuery->bindParam(":username", $Username);
+    $insertQuery->bindParam(":tanggal", $date);
+    $insertQuery->bindParam(":isi", $isi);
+    $insertQuery->execute();
+    
+    header("location: $base_url/detailBerita.php?judul=$judul");
