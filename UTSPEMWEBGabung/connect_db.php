25b39052dcb224f5d71373590cccb2c7ef44cd5b
FlxNg <91363258+FlxNg@users.noreply.github.com>
2021-10-10 19:00:17 +0700

    Update connect_db.php

--- UTSPEMWEBGabung/connect_db.php
+++ UTSPEMWEBGabung/connect_db.php
@@ -11,3 +11,18 @@
     $base_url = 'http://localhost/UTSPEMWEBGabung';
     $db = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
 ?>
+
+<!-- Ini yang untuk hostingan -->
+<?php
+//     if(!isset($_SESSION)){ 
+//         session_start();
+//     };
+
+//     $host = "localhost";
+//     $username = "id17737508_webuts";
+//     $dbname = "id17737508_web_uts";
+//     $password = "p*<3xe)2#s@GSs\u";
+
+//     $base_url = 'https://closest-streets.000webhostapp.com/UTSPEMWEBGabung';
+//     $db = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
+?>
