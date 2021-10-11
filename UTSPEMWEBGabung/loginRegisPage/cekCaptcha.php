dad4d802656c781dafa9e9eba913f82bb23d7fe3
tesalonikabigail <91363224+tesalonikabigail@users.noreply.github.com>
2021-10-11 12:57:56 +0700

    Delete cekCaptcha.php

--- UTSPEMWEBGabung/loginRegisPage/cekCaptcha.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if ($_SESSION["code"] != $_POST["kodecaptcha"]) {
-        $_SESSION['error_login_message'] = 'Captcha yang dimasukkan salah';
-        header("location: $base_url/loginRegisPage/loginRegister.php");
-    }
-?>
